95399b7e63e84bb56f6f55e6d139b2087e4e828b
liikliik <pattyajestas@gmail.com>
2026-04-29 10:59:22 +0800

    Fern Add sprite, script and game object

---
+++ Assets/Scripts/FernControl.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FernControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hi peeps");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
