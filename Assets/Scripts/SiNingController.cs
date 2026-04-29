7cb559b646dc1d686fe95a42bcfebd98f801d320
sn462 <snweee2347@gmail.com>
2026-04-29 10:59:16 +0800

    Si Ning added sprite, script and game object

---
+++ Assets/Scripts/SiNingController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SiNingController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Penis head");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
