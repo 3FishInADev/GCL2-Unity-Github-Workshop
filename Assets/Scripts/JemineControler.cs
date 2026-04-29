870f7ae07e93e6535f288e87edb1d63fa9d242f3
3FishInACoat <jemineliau2008@gmail.com>
2026-04-29 10:28:11 +0800

    Jemine add GroundSprite and JemineScript

---
+++ Assets/Scripts/JemineControler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class JemineControler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("HI I want to sleep and I am the Floor, Yahoo!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
